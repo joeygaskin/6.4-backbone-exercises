faf9aa1242ddec22aefc09d576668e0fc1d2165c
Joey <me@joeygaskin.com>
2015-10-13 23:25:32 -0400

    Update 2015-10-14T03:25:31.203Z

--- app/scripts/a.js
+++
@@ -1,5 +0,0 @@
-
-$(document).ready(function(){
-  // prepend the contents of `app/templates/a.index.hbs` into `body`
-  $('#container').append(JST.aindex());
-});
--- app/scripts/a/views/collection-view.js
+++
@@ -1,9 +0,0 @@
-import Index from 'views/index';
-
-export default Index.extend({
-
-  initialize: function() {
-    this.listenTo(this.collection, 'add remove', this.render);
-  },
-
-});
